73015aaaf0cb34671f260a34f146f5ab5b9f1ea5
Edward Gillard <edwardgillard@edwards-mbp.mynet>
2020-04-30 17:50:35 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
 import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import Home from './components/common/Home'
+import Navbar from './components/common/Navbar'
+import ShowNews from './components/items/ShowNews'
+import Business from './components/items/categories/Business'
+import Entertainment from './components/items/categories/Entertainment'
+import General from './components/items/categories/General'
+import Health from './components/items/categories/Health'
+import Science from './components/items/categories/Science'
+import Sports from './components/items/categories/Sports'
+import Technology from './components/items/categories/Technology'
+
 
 const App = () => {
+
+
   return (
-    <h1>Hello World</h1>
+    <BrowserRouter>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/news/business" component={Business} />
+        <Route path="/news/entertainment" component={Entertainment} />
+        <Route path="/news/general" component={General} />
+        <Route path="/news/health" component={Health} />
+        <Route path="/news/science" component={Science} />
+        <Route path="/news/sports" component={Sports} />
+        <Route path="/news/technology" component={Technology} />
+        <Route path="/news" component={ShowNews} />
+      </Switch>
+
+    </BrowserRouter>
   )
 }
 
---
+++ src/lib/api.js
@@ -0,0 +1,13 @@
+import axios from 'axios'
+
+const apiKey = '&apiKey=6e145feb0c664217b76b233d8c8d7366'
+
+const newsUrl = 'https://newsapi.org/v2/top-headlines'
+
+export const getAllNews = (country) => {
+  return axios.get(`${newsUrl}?country=${country}${apiKey}`)
+}
+
+export const filterCategory = (country, category) => {
+  return axios.get(`${newsUrl}?country=${country}&category=${category}${apiKey}`)
+}
