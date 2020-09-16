7642b7de136d25a6780c3085dfa0482ed55950a8
Edward Gillard <edwardgillard@192.168.1.106>
2020-09-16 12:38:17 +0100

    updated error message and readme

--- src/components/common/ErrorPage.js
+++ src/components/common/ErrorPage.js
@@ -6,7 +6,7 @@ const ErrorPage = () => {
     <>
       <Navbar />
       <h1 className="error-page">Something went wrong</h1>
-      <p> This API was restricted on deployment. Please visit <a>https://github.com/EdwardGillard/SEI-project-two</a>, clone the repository and run using yarn start.</p>
+      <p className="error-message"> This API was restricted on deployment. Please visit <a>https://github.com/EdwardGillard/SEI-project-two</a>, clone the repository and run using yarn start.</p>
     </>
   )
 }
